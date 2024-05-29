a83d6f05714bbea564dd8e78e984fd3e748b51a1
Lorenzo Andreolli Pilotti <l23pilotti@gmail.com>
2024-05-28 21:41:45 -0300

    Update and rename MyFirst.java to First.java

---
+++ First.java
@@ -0,0 +1,12 @@
+import java.lang.*;
+
+public class First
+{
+	public static void main (String arg[])
+ 	{
+		System.out.println("Hello world");
+		System.out.println(arg[0]);
+		System.out.println(arg[1]);
+	}
+
+}
